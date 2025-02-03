e4fada85ffbf3f03089b2597f822a0b7882de18a
Hector B <hectorbg303@gmail.com>
2025-02-03 18:18:07 +0100

    2619. Array Prototype Last

---
+++ 2619. Array Prototype Last/array_prototype_last.test.js
@@ -0,0 +1,31 @@
+import { it, expect } from "vitest";
+
+import { last } from "./array_prototype_last";
+
+// Input: nums = [null, {}, 3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+
+it("Calling nums.last() should return the last element: 3.", () => {
+  const nums = [null, {}, 3];
+
+  Array.prototype.last = last;
+
+  const result = nums.last();
+
+  expect(result).toBe(3);
+});
+
+// Input: nums = []
+// Output: -1
+// Explanation: Because there are no elements, return -1.
+
+it("Calling nums.last() should return the last element: 3.", () => {
+  const nums = [];
+
+  Array.prototype.last = last;
+
+  const result = nums.last();
+
+  expect(result).toBe(-1);
+});
